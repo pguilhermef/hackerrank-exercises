d834195c41258c69784622d57a3cc41543ecdf51
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-24 21:37:50 -0300

    feat: Create the problem statement.

--- problem-solving/birthday-cake-candles.js
+++ problem-solving/birthday-cake-candles.js
@@ -1,3 +1,9 @@
+// I was given an array of candles, where each candle had a height represented by a number. The taller the candle, the higher the number in the corresponding array position.
+// The challenge was to determine which candles had the greatest height and return the count of candles with that same height.
+
+// Eu recebi um array de velas, onde cada vela tinha uma altura representada por um número. Quanto mais alta a vela, maior o número na posição correspondente do array.
+// O desafio era determinar quais velas tinham a maior altura e retornar a contagem de velas com a mesma altura.
+
 const candles = [18, 90, 90, 13, 90, 75, 90, 8, 90, 43]
 
 function birthdayCakeCandles(candles) {
