7139b4a53b4b3b27ab77275db77b2837a808d11b
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-31 22:44:10 -0300

    feat: Solve the day-1 of 10 days of javascript

---
+++ 10-days-of-javascript/day-1-hello-world.js
@@ -0,0 +1,10 @@
+// I needed print 'Hello, world!' and the phrase of parameter
+
+// Eu precisava printar 'Hello, world' e a frase do parametro
+
+function greeting(parameterVariable) {
+  console.log('Hello, World!');  
+  console.log(parameterVariable);
+}
+
+greeting("I'm Paulo!");
